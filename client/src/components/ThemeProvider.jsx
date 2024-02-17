37372fdd30d472a0fdfbb4601fb08285114433d6
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-16 18:08:59 -0600

    Add oauth and theme

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from 'react-redux';
 
-const ThemeProvider = () => {
-  return (
-    <div>ThemeProvider</div>
-  )
-}
+const ThemeProvider = ({ children }) => {
+    const { theme } = useSelector((state) => state.theme);
 
-export default ThemeProvider
+    return (
+        <div className={theme}>
+            <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen'>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default ThemeProvider;
