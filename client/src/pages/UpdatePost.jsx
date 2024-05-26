1a19861fdcdc00ed0c140d25ec0dde98c6fda3c2
Aaliyah Montgomery <93296412+Aaliyah1699@users.noreply.github.com>
2024-05-26 09:52:23 -0500

    Update UpdatePost.jsx

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -143,7 +143,7 @@ const UpdatePost = () => {
                         <option value='uncategorized'>Select a category</option>
                         <option value='poem'>Poems</option>
                         <option value='tech'>Tech</option>
-                        <option value='random'>Random</option>
+                        <option value='astrology'>Astrology</option>
                     </Select>
                 </div>
                 <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
