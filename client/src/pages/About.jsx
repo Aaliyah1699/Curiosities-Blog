fe85e956381acb7ec5b6367f3bec1de1d3c0360a
Aaliyah Montgomery <93296412+Aaliyah1699@users.noreply.github.com>
2024-06-30 16:08:50 -0500

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
             <div className='max-w-2xl mx-auto p-3 text-center'>
                 <div>
                     <h1 className='text-3xl font font-semibold text-center my-7'>
-                        About Curiosities of Aaliyah
+                        About The Curiosities of Aaliyah
                     </h1>
                     <div className='text-md text-gray-500 flex flex-col gap-6'>
                         <p>
