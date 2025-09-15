a2062ed7679532fc60c3b3b538eadf0d16f9c61a
Aaliyah Montgomery <Adestanym@gmail.com>
2025-09-15 17:18:40 -0500

    Update CallToAction.jsx

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -8,35 +8,24 @@ const CallToAction = () => {
                     For a deeper look at what I can do
                 </h2>
                 <p className='text-gray-500 my-2 capitalize'>
-                    I invite you to explore my portfolios
+                    I invite you to explore my portfolio
                 </p>
                 <Button
                     gradientDuoTone='pinkToOrange'
                     className='rounded-tl-xl rounded-bl-none mb-2'
                 >
                     <a
-                        href='https://aaliyahm-portfolio.netlify.app/'
+                        href='https://aaliyahs-portfolio.netlify.app/'
                         target='_blank'
                         rel='noopener noreferrer'
                     >
-                        Software Development Portfolio
-                    </a>
-                </Button>
-                 <Button
-                    gradientDuoTone='pinkToOrange'
-                    className='rounded-tl-xl rounded-bl-none mt-2'
-                >
-                    <a
-                        href='https://aaliyah-da-portfolio.netlify.app/'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        Data Analysis Portfolio
+                        Portfolio 💐
                     </a>
                 </Button>
             </div>
             <div className='p-7 flex-1'>
-                <img src='https://binarapps.com/wp-content/uploads/2021/09/Top-10-Programming-Languages-of-the-Future.png' />
+                <img src='https://i.pinimg.com/736x/1f/46/8c/1f468c1437ccd23228a00e26cf1377c7.jpg' />
+{/*                 <img src='https://binarapps.com/wp-content/uploads/2021/09/Top-10-Programming-Languages-of-the-Future.png' /> */}
             </div>
         </div>
     );
